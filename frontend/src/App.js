5e02d41de1b3631b841860c780d3f3541bba2224
Pankaj sharam <Pankajsharma1997>
2024-04-03 15:52:19 +0530

    make routes for nav links

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,26 @@
 
 import './App.css';
-
+import Nav from './components/Nav';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Footer from './components/Footer';
+import SignUp from './components/SignUp';
 function App() {
   return (
     <div className="App">
-     <h1> E-Dashboard </h1>
+      <BrowserRouter>
+      <Nav /> 
+      <Routes> 
+       <Route path ="/"    element={<h1> Product listing  Component </h1>}/> 
+       <Route path ="/add" element ={ <h1> Add Product Component </h1>}/>
+       <Route path ="/update" element = { <h1> Update Product Component </h1>}/>
+       <Route path ="/logout" element ={ <h1> Logout Sucessfully </h1>} />
+       <Route path ="/profile" element ={<h1> Your Profile Page Component </h1>} /> 
+       <Route path = "/SignUp" element = {<SignUp />} />
+
+      </Routes>
+     
+     </BrowserRouter>
+     <Footer/> 
     </div>
   );
 }
