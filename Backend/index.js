4d1d11e9da7881659993ddf247a072ffde5242da
Pankaj sharam <Pankajsharma1997>
2024-04-04 11:24:08 +0530

    POST API for Reagister a user

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,15 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
+const User = require("./DB/Users");
+require("./DB/Config");
 
 const app = express();
-
-app.get('/',(req,resp) =>{
-    resp.send(" App is working ");
+app.use(express.json());
+app.use(cors());
+app.post("/register", async (req, resp) => {
+  let user = new User(req.body);
+  let result = await user.save();
+  resp.send(result);
 });
 
 app.listen(5000);
